c4c19cded54a853620f4e686663f5d6f6c256964
roninzandy <suddenlyandy@gmail.com>
2023-08-15 09:15:25 +0600

    улучшена структура кода

--- main.py
+++ main.py
@@ -8,108 +8,88 @@ from selenium.webdriver.common.keys import Keys
 from fake_useragent import UserAgent
 from selenium.webdriver.chrome.options import Options
 
-# headers = {
-#     'Accept': '*/*',
-#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
-# }
-# url = 'https://hh.kz/search/vacancy?no_magic=true&L_save_area=true&text=python&excluded_text=&area=160&salary=&currency_code=KZT&experience=doesNotMatter&order_by=publication_time&search_period=0&items_on_page=100'
-#
+headers = {
+    'Accept': '*/*',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
+}
 
-# options = webdriver.ChromeOptions()
-# options.add_argument(f'--headers="{headers}"')
-# driver = webdriver.Chrome(options=options)
-# options.add_argument(f"user-agent={headers['User-Agent']}")
+options = webdriver.ChromeOptions()
+options.add_argument(f'--headers="{headers}"')
+driver = webdriver.Chrome(options=options)
+options.add_argument(f"user-agent={headers['User-Agent']}")
+def get_url(p):
+    return f'https://hh.kz/search/vacancy?text=python&salary=&no_magic=true&ored_clusters=true&order_by=publication_time&enable_snippets=true&excluded_text=&area=160&page={p}'
+response = requests.get(url=get_url(0), headers=headers)
 
-# for i in range(5):
-#     try:
-#         driver.get(f'https://hh.kz/search/vacancy?text=python&salary=&no_magic=true&ored_clusters=true&order_by=publication_time&enable_snippets=true&excluded_text=&area=160&page={i}')
-#         sleep(5)
-#         with open(f'selenium_data_2/page_{i+1}.html', 'w', encoding='utf-8') as file:
-#             file.write(driver.page_source)
-#             print(f'Страница {i+1} сохранена.')
-#             sleep(5)
-#     except Exception as ex:
-#         print(ex)
-#     finally:
-#         pass
-#
-# driver.close()
-# driver.quit()
+src = response.text
+with open('test.html', 'w', encoding='utf-8') as file:
+    file.write(src)
 
-#
-# response = requests.get(url=url, headers=headers)
-# src = response.text
-#
-# with open('test.html', 'w', encoding='utf-8') as file:
-#     file.write(src)
+with open('test.html', encoding='utf-8') as file:
+    src = file.read()
+    soup = BeautifulSoup(src, 'lxml')
+
+    page_numbers = int(list(soup.find('div', class_='pager'))[-2].text)
+    print(f'Всего страниц по данному запросу: {page_numbers}')
+
+for i in range(1, page_numbers + 1):
+    try:
+        driver.get(f'https://hh.kz/search/vacancy?text=python&salary=&no_magic=true&ored_clusters=true&order_by=publication_time&enable_snippets=true&excluded_text=&area=160&page={i-1}')
+        sleep(5)
+        with open(f'selenium_data/page_{i}.html', 'w', encoding='utf-8') as file:
+            file.write(driver.page_source)
+            print(f'Страница {i} сохранена.')
+            sleep(5)
+    except Exception as ex:
+        print(ex)
+    finally:
+        pass
+
+driver.close()
+driver.quit()
 
 def get_data():
-    with open('test.html', encoding='utf-8') as file:
-        src = file.read()
-        soup = BeautifulSoup(src, 'lxml')
+    g_count = 0
+    global page_numbers
+    for j in range(1, page_numbers + 1):
+        with open(f'selenium_data/page_{j}.html', encoding='utf-8') as file:
+            src_data = file.read()
+            print(f'Начинается сбор данных со страницы {j}.')
+            print('_'*20)
+            soup_data = BeautifulSoup(src_data, 'lxml')
+            data = soup_data.find_all('div', class_='vacancy-serp-item-body__main-info')
+            for d in data:
 
-        page_numbers = int(list(soup.find('div', class_='pager'))[-2].text)
-        print(page_numbers)
+                try:
+                    title = d.find('a', class_='serp-item__title').text
+                except:
+                    title = 'Должность не указана'
 
-        pages = [page for page in range(page_numbers)]
-        print(pages)
+                try:
+                    salary = d.find('span', class_='bloko-header-section-2').text
+                except Exception as ex:
+                    salary = 'Зарплата не указана'
 
-        urls = {}
-        url = 'https://hh.kz/search/vacancy?no_magic=true&L_save_area=true&text=python&excluded_text=&area=160&salary=&currency_code=KZT&experience=doesNotMatter&order_by=publication_time&search_period=0&items_on_page=100&page='
-        for page in range(len(pages)):
-            urls[page+1] = f'{url}{page}'
-        print(urls)
+                try:
+                    company = d.find('a', class_='bloko-link').text
+                except Exception as ex:
+                    company = 'Компания не указана'
 
-        g_count = 0
-        #for key, value in urls.items():
-        for key in range(5):
-            print(key)
+                try:
+                    location = d.find('div', attrs={'data-qa': 'vacancy-serp__vacancy-address'}).text.split(',')[0]
+                except Exception as ex:
+                    location = 'Место работы не указано'
 
-            # response = requests.get(url=value, headers=headers)
-            # src = response.text
-            # with open(f'data/page_{key}.html', 'w', encoding='utf-8') as file:
-            #     file.write(src)
-            #     print(f'Страница {key} сохранена.')
-            #with open(f'selenium_data/page_{key}.html', encoding='utf-8') as file:
-            with open(f'selenium_data/page_{key+1}.html', encoding='utf-8') as file:
-                src = file.read()
-                #print(f'Начинается сбор данных со страницы {key + 1}.')
-                print(f'Начинается сбор данных со страницы {key+1}.')
-                print('_'*20)
-                soup = BeautifulSoup(src, 'lxml')
-                data = soup.find_all('div', class_='vacancy-serp-item-body__main-info')
-                for d in data:
+                g_count += 1
+                print(f'Обрабатывается запись: {g_count}')
+                print('#' * 20)
+                print(title)
+                print(salary)
+                print(company)
+                print(location)
+                print('#'*20)
 
-                    try:
-                        title = d.find('a', class_='serp-item__title').text
-                    except Exception as ex:
-                        title = 'Должность не указана'
-
-                    try:
-                        salary = d.find('span', class_='bloko-header-section-2').text
-                    except Exception as ex:
-                        salary = 'Зарплата не указана'
-
-                    try:
-                        company = d.find('a', class_='bloko-link').text
-                    except Exception as ex:
-                        company = 'Компания не указана'
-
-                    try:
-                        location = d.find('div', attrs={'data-qa': 'vacancy-serp__vacancy-address'}).text.split(',')[0]
-                    except Exception as ex:
-                        location = 'Место работы не указано'
-
-                    g_count += 1
-                    print(f'Обрабатывается запись: {g_count}')
-                    print('#' * 20)
-                    print(title)
-                    print(salary)
-                    print(company)
-                    print(location)
-                    print('#'*20)
-
-                    #time.sleep(1)
+                #time.sleep(1)
 
 def main():
     get_data()
@@ -117,6 +97,5 @@ def main():
 if __name__ == '__main__':
    main()
 
-    #всего вакансий: 225
 
 
