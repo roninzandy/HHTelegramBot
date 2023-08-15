d810a1ba2548abda7166da37b71f32a038c0e27f
roninzandy <suddenlyandy@gmail.com>
2023-08-15 07:10:00 +0600

    отображение обработки записей

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import time
+
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
-url = 'https://hh.kz/search/vacancy?area=160&enable_snippets=true&excluded_text=&no_magic=true&ored_clusters=true&text=python&order_by=publication_time'
+url = 'https://hh.kz/search/vacancy?no_magic=true&L_save_area=true&text=python&excluded_text=&area=160&salary=&currency_code=KZT&experience=doesNotMatter&order_by=publication_time&search_period=0&items_on_page=100'
 headers = {
     'Accept': '*/*',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
@@ -25,21 +30,65 @@ def get_data():
         print(pages)
 
         urls = {}
-        url = 'https://hh.kz/search/vacancy?area=160&enable_snippets=true&excluded_text=&no_magic=true&ored_clusters=true&text=python&order_by=publication_time&page='
+        url = 'https://hh.kz/search/vacancy?no_magic=true&L_save_area=true&text=python&excluded_text=&area=160&salary=&currency_code=KZT&experience=doesNotMatter&order_by=publication_time&search_period=0&items_on_page=100&page='
         for page in range(len(pages)):
             urls[page+1] = f'{url}{page}'
         print(urls)
-
+        count = 0
+        g_count = 0
         for key, value in urls.items():
-            response = requests.get(url=value, headers=headers)
-            src = response.text
-            with open(f'data/page_{key}.html', 'w', encoding='utf-8') as file:
-                file.write(src)
-                print(f'Страница {key} сохранена.')
+
+            if count == 0:
+                response = requests.get(url=value, headers=headers)
+                src = response.text
+                with open(f'data/page_{key}.html', 'w', encoding='utf-8') as file:
+                    file.write(src)
+                    print(f'Страница {key} сохранена.')
+                with open(f'data/page_1.html', encoding='utf-8') as file:
+                    src = file.read()
+                    print(f'Начинается сбор данных со страницы {key}.')
+                    print('_'*20)
+                    soup = BeautifulSoup(src, 'lxml')
+                    data = soup.find_all('div', class_='vacancy-serp-item-body__main-info')
+                    for d in data:
+                        #if 0 <= count < 4:
+                            #count += 1
+                            try:
+                                title = d.find('a', class_='serp-item__title').text
+                            except Exception as ex:
+                                title = 'Должность не указана'
+
+                            try:
+                                salary = d.find('span', class_='bloko-header-section-2').text
+                            except Exception as ex:
+                                salary = 'Зарплата не указана'
+
+                            try:
+                                company = d.find('a', class_='bloko-link').text
+                            except Exception as ex:
+                                company = 'Компания не указана'
+
+                            try:
+                                location = d.find('div', attrs={'data-qa': 'vacancy-serp__vacancy-address'}).text.split(',')[0]
+                            except Exception as ex:
+                                location = 'Место работы не указано'
+
+                            print(f'Обрабатывается запись: {g_count}')
+                            print('#' * 20)
+                            print(title)
+                            print(salary)
+                            print(company)
+                            print(location)
+                            print('#'*20)
+                            g_count += 1
+                            #time.sleep(1)
+
 def main():
     get_data()
 
 if __name__ == '__main__':
     main()
+
+    #всего вакансий: 225
 
 
